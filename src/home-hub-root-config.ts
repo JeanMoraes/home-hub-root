ce488c2cf015907e88f327209dbcf8351db38b09
Jean Moraes <jean.moraes@t10.digital>
2024-06-18 17:31:16 +0100

    chore: configurações iniciais do orquestrador dos microfrontends

---
+++ src/home-hub-root-config.ts
@@ -0,0 +1,21 @@
+import { registerApplication, start, LifeCycles } from "single-spa";
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+registerApplication({
+  name: "@home-hub/react-navbar",
+  app: () => System.import<LifeCycles>("@home-hub/react-navbar"),
+  activeWhen: (location) => location.pathname.includes('/')
+  // activeWhen: ["/"]
+});
+
+start({
+  urlRerouteOnly: true,
+});
